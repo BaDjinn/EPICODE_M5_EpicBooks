1596bb535a33adccc97f946604704d477d5c9d43
BaDjinn <g.sottovia@yahoo.com>
2023-09-18 20:09:58 +0200

    .

--- src/components/lastestRelease/LastestRelease.jsx
+++ src/components/lastestRelease/LastestRelease.jsx
@@ -5,27 +5,29 @@ import { nanoid } from "nanoid";
 import { Library } from "../../contexts/BooksContext";
 import MySpinner from "../spinner/MySpinner";
 
-const LastestRelease = ()=>{
-  const {books, loading, errors} = useContext(Library)
-  return (
-    <Container>
-      <Row>
-        {loading && <MySpinner />}
-        {books && !loading && !errors &&
-          books.map((book) => {
-            return (
-              <Col key={nanoid()} xs={12} sm={6} md={3}>
-                <MyCard
-                  url={book.img}
-                  title={book.title}
-                  text={book.category}
-                />
-              </Col>
-            );
-          })}
-      </Row>
-    </Container>
-  );
-}
+const LastestRelease = () => {
+	const { books, loading, errors } = useContext(Library);
+	return (
+		<Container>
+			<Row>
+				{loading && <MySpinner />}
+				{books &&
+					!loading &&
+					!errors &&
+					books.map((book) => {
+						return (
+							<Col key={nanoid()} xs={12} sm={6} md={3}>
+								<MyCard
+									url={book.img}
+									title={book.title}
+									text={book.category}
+								/>
+							</Col>
+						);
+					})}
+			</Row>
+		</Container>
+	);
+};
 
 export default LastestRelease;
